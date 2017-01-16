0b6fa6a6be0561449f267225bc643c5e51362170
Andrew McCluskey <andrew@ajmccluskey.com>
2017-01-16 20:39:26 +1000

    Take argument to rename-photos-dir

--- rename-photo-dirs.hs
+++ rename-photo-dirs.hs
@@ -3,18 +3,27 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import Turtle
+import Turtle as T
 
-main = doIt
+data Fail = NoArgs
 
-doIt :: IO ()
-doIt = do
-  dirs <- join $ fold (ls "..") filterDirs
+main = do
+  args <- arguments
+  case args of
+    (x:_) -> doIt . fromText $ x
+    _     -> whoopsie NoArgs
+
+doIt :: T.FilePath -> IO ()
+doIt fp = do
+  dirs <- join $ fold (ls fp) filterDirs
   sequence $ fmap (putStrLn . show) dirs
   return ()
 
-filterDirs :: Fold Turtle.FilePath (IO [Turtle.FilePath])
+filterDirs :: Fold T.FilePath (IO [T.FilePath])
 filterDirs = Fold addIfDir (return []) id
   where addIfDir dirs fp = do
           isDir <- testdir fp
           if isDir then fmap (fp :) dirs else dirs
+
+whoopsie :: Fail -> IO ()
+whoopsie NoArgs = putStrLn "Expecting album dir as argument"
